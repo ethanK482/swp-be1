3c68eced7d6235987d51b02b3623b0f4d3fbdf00
baophan894 <monkeyold113@gmail.com>
2024-06-25 13:03:49 +0700

    upload and get report by day

---
+++ src/main/java/swp/group2/swpbe/report/ReportRepository.java
@@ -0,0 +1,13 @@
+package swp.group2.swpbe.report;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import swp.group2.swpbe.report.entities.Report;
+
+import java.util.List;
+
+
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Integer> {
+    List<Report> findAllByOrderByReportDateDesc();
+}
